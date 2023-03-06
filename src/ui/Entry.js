3556d61c7687e9b53d7229094b65384d7ee758e0
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-03-06 11:22:31 -0300

    WIP filled the rest of the file content data

--- src/ui/Entry.js
+++ src/ui/Entry.js
@@ -1,15 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+// import { getLog } from '../util/log';
+
 import { buildCell, buildRow, buildTable } from '../util/html';
 
 import './Entry.css';
 
-// function componentDidMount(props, dispatch) {
-// }
+// const log = getLog('Entry.');
 
-// function componentDidUpdate(props, prevProps, dispatch) {
-// }
+function componentDidMount(props/*, dispatch*/, valueNeutralField, valueEnglishField, commentNeutralField, commentEnglishField) {
+	if (valueNeutralField && (valueNeutralField.value !== (props.entry[''].value || ''))) {
+		valueNeutralField.value = props.entry[''].value || '';
+	}
+	if (valueEnglishField && (valueEnglishField.value !== (props.entry['en-US'].value || ''))) {
+		valueEnglishField.value = props.entry['en-US'].value || '';
+	}
+	if (commentNeutralField && (commentNeutralField.value !== (props.entry[''].comment || ''))) {
+		commentNeutralField.value = props.entry[''].comment || '';
+	}
+	if (commentEnglishField && (commentEnglishField.value !== (props.entry['en-US'].comment || ''))) {
+		commentEnglishField.value = props.entry['en-US'].comment || '';
+	}
+}
+
+function componentDidUpdate(props, prevProps/*, dispatch*/, valueNeutralField, valueEnglishField, commentNeutralField, commentEnglishField) {
+	componentDidMount(props, valueNeutralField, valueEnglishField, commentNeutralField, commentEnglishField);
+}
 
 const pt_BR_flag = <img height='16' width='16' src='https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg' />;
 const en_flag = <img height='16' width='16' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/English_language.svg' />;
@@ -24,16 +41,23 @@ function usePrevious(value) {
 
 function Entry(props) {
 
+	// log('Entry', { props });
+
 	const didMountRef = useRef(false);
 
 	const prevProps = usePrevious(props);
 
+	const [valueNeutralField, setValueNeutralField] = useState(null);
+	const [valueEnglishField, setValueEnglishField] = useState(null);
+	const [commentNeutralField, setCommentNeutralField] = useState(null);
+	const [commentEnglishField, setCommentEnglishField] = useState(null);
+
 	useEffect(() => {
 		if (didMountRef.current) {
-			// componentDidUpdate(props, prevProps, dispatch);
+			componentDidUpdate(props, prevProps/*, dispatch*/, valueNeutralField, valueEnglishField, commentNeutralField, commentEnglishField);
 		} else {
 			didMountRef.current = true;
-			// componentDidMount(props, dispatch);
+			componentDidMount(props, /*dispatch, */valueNeutralField, valueEnglishField, commentNeutralField, commentEnglishField);
 		}
 	});
 
@@ -55,6 +79,7 @@ function Entry(props) {
 			src='https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg'
 		/><input
 			className='value_neutral'
+			ref={ref => { if (ref) { setValueNeutralField(ref); } }}
 		/><img
 			className='value_english'
 			height='16'
@@ -62,8 +87,9 @@ function Entry(props) {
 			src='https://upload.wikimedia.org/wikipedia/commons/0/0b/English_language.svg'
 		/><input
 			className='value_english'
+			ref={ref => { if (ref) { setValueEnglishField(ref); } }}
 		/><span
-			className='comment   text_align_center'
+			className='comment text_align_center'
 		>C</span><img
 			className='comment_neutral'
 			height='16'
@@ -71,6 +97,7 @@ function Entry(props) {
 			src='https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg'
 		/><input
 			className='comment_neutral'
+			ref={ref => { if (ref) { setCommentNeutralField(ref); } }}
 		/><img
 			className='comment_english'
 			height='16'
@@ -78,8 +105,8 @@ function Entry(props) {
 			src='https://upload.wikimedia.org/wikipedia/commons/0/0b/English_language.svg'
 		/><input
 			className='comment_english'
+			ref={ref => { if (ref) { setCommentEnglishField(ref); } }}
 		/></div>;
-
 }
 
 export default Entry;
